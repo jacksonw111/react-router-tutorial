d7b79d20f34dca56a01838c96004729bea4ba12b
jackson.wen <jackson.wen@xm.wonder.com>
2024-01-24 11:04:21 +0800

    dynamic router

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Layout } from "../layout/Layout";
 import { ErrorPage } from "../pages/error/ErrorPage";
+import { Contact } from "../pages/contect/Contact";
 
 export const router = createBrowserRouter([
   {
     path: '/', 
     element: <Layout />,
     errorElement: <ErrorPage />
+  },
+  {
+    path: 'contacts/:contactId',
+    element: <Contact />
   }
 ])
