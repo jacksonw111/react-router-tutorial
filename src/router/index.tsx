68f6d2c4f28832431134ed76ffa899d84aa70067
jackson.wen <jackson.wen@xm.wonder.com>
2024-01-24 10:42:31 +0800

    simple error page

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,8 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
+import { Layout } from "../layout/Layout";
+import { ErrorPage } from "../pages/error/ErrorPage";
 
 export const router = createBrowserRouter([
   {
     path: '/', 
-    element: <div>hello</div>
+    element: <Layout />,
+    errorElement: <ErrorPage />
   }
 ])
